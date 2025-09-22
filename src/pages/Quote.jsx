c215851ba4e26270eabfd444f018388ac5a5cd24
Poyraz Karadogan <148263515+PoyrazKaradogan@users.noreply.github.com>
2025-09-22 18:02:43 +0300

    aa

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Layout from "../layout/Layout";
+import Layout from ".././layout/layout";
 import MailForm from "../components/MailForm";
 import ContactCard from "../components/ContactCard";
 import MapEmbed from "../components/MapEmbed";
